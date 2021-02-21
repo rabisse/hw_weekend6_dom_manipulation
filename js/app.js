9f08e0ba0dffa350bf1f3ebe2d016b9caf26c9ae
rabisse <rabisse@gmail.com>
2021-02-21 14:48:30 +0000

    initial commit

---
+++ js/app.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const newDrinkForm = document.querySelector('#new-drink-form')
+    newDrinkForm.addEventListener('submit', handleNewDrinkSubmit)
+
+
+})
+
+const handleNewDrinkSubmit = function(evt) {
+    evt.preventDefault()
+
+    const drinkListItem = createDrinkListItem(evt.target)
+    const drinkList = document.querySelector('#drink-list')
+    drinkList.appendChild(drinkListItem)
+    evt.target.reset()
+}
+
+const createDrinkListItem = function(form) {
+    const drinkListItem = document.createElement('li')
+    drinkListItem.classList.add('drink-list-item')
+
+    const name = document.createElement('h3')
+    name.textContent = form.name.value
+    drinkListItem.appendChild(name)
+
+    const carbonated = document.createElement('p')
+    carbonated.textContent = `Carbonated: ${form.carbonated.value}`
+    drinkListItem.appendChild(carbonated)
+
+    const alcohol = document.createElement('p')
+    alcohol.textContent = `Contains alcohol: ${form.alcohol.value}`
+    drinkListItem.appendChild(alcohol)
+
+    const caffeine = document.createElement('p')
+    caffeine.textContent = `Caffeine level: ${form.caffeine.value}`
+    drinkListItem.appendChild(caffeine)
+
+
+
+}
+
+
+
